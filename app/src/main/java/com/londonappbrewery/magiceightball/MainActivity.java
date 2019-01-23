2c95638451f8bcdf688e128d64164298684aa98e
simo <simolabsoft@gmail.com>
2019-01-23 18:48:52 +0100

    assets added

--- app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
@@ -2,6 +2,11 @@ package com.londonappbrewery.magiceightball;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button askBtn  = findViewById(R.id.askBtn);
+        final ImageView ball = findViewById(R.id.image_ball);
+
+        final int[] balls  = new int[]{
+                R.drawable.ball1,
+                R.drawable.ball2,
+                R.drawable.ball3,
+
+        };
+        askBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Random randomGen = new Random();
+                int numberRandom = randomGen.nextInt(3);
+                ball.setImageResource(balls[numberRandom]);
+            }
+        });
+
     }
 }
